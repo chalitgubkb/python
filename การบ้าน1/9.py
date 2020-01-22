b2c73cea07973e5a114d28571f3c1a5cd5a160a1
chalitgubkb <birdbkbbird@gmail.com>
2020-01-23 00:12:31 +0700

    ทำข้อ 9

---
+++ การบ้าน1/9.py
@@ -0,0 +1,12 @@
+x = input('Enter Your Char : ')
+
+if x == 'M':
+    print('Individual is married')
+elif x == 'S':
+    print('Individual is single')
+elif x == 'D':
+    print('Individual is divorced')
+elif x == 'W':
+    print('Individual is widowed')
+else:
+    print('An Invalid code was entered')
