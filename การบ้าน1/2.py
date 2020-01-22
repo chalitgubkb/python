440e6e86149296b583a1c910d4c4b01dcf0682ee
chalitgubkb <birdbkbbird@gmail.com>
2020-01-23 03:55:10 +0700

    รับค่า3ค่าแล้วหาค่าเฉลี่ยทั้ง3ค่า

--- การบ้าน1/2.py
+++ การบ้าน1/2.py
@@ -1,6 +1,6 @@
-x = int(input('Enter Your Num X : '))
-y = int(input('Enter Your Num Y : '))
-z = int(input('EnTER Your Num Z : '))
+x = int(input('Enter Your Num X :'))
+y = int(input('Enter Your Num Y :'))
+z = int(input('EnTER Your Num Z :'))
 
 avg = (x+y+y)/3
 
