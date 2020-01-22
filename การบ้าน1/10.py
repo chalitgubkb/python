187fd8ccc030be44ec0dc505df2d4cfa98c0a094
chalitgubkb <birdbkbbird@gmail.com>
2020-01-23 00:42:02 +0700

    ทำข้อ 10

---
+++ การบ้าน1/10.py
@@ -0,0 +1,26 @@
+p = int(input('Enter Your Price : '))
+
+if p >= 50000:
+    v = 16
+    vat = (p/100)*v+575
+    print('Salary %d %% = %d ' %(v,vat))
+elif p < 50000 and p >= 40000:
+    v = 14
+    vat = (p/100)*v+550
+    print('Salary %d %% = %d ' %(v,vat))
+elif p < 40000 and p >= 30000:
+    v = 12
+    vat = (p/100)*v+525
+    print('Salary %d %% = %d ' %(v,vat))
+elif p < 30000 and p >= 20000:
+    v = 9
+    vat = (p/100)*v+500
+    print('Salary %d %% = %d ' %(v,vat))
+elif p < 20000 and p >= 10000:
+    v = 5
+    vat = (p/100)*v+450
+    print('Salary %d %% = %d ' %(v,vat))
+elif p < 10000:
+    v = 3
+    vat = (p/100)*v+400
+    print('Salary %d %% = %d ' %(v,vat))
