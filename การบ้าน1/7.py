7a71215f9df3f60670dac96ac37e298431a879c1
chalitgubkb <birdbkbbird@gmail.com>
2020-01-23 00:00:00 +0700

    ทำข้อ 7

---
+++ การบ้าน1/7.py
@@ -0,0 +1,10 @@
+salary = float(input('Enter Your Salary : '))
+
+if salary <= 20000:
+    v = 2
+    vat = (salary/100)*v
+    print('Vat %d %% = %.2f' %(v,vat))
+elif salary > 20000:
+    v = 2.5
+    vat = (salary/100)*v
+    print('Vat %.2f %% = %.2f' %(v,vat))
