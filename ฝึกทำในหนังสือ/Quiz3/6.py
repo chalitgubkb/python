b8a01fbae780e3e59253beca2389e179c6c3642f
chalitgubkb <birdbkbbird@gmail.com>
2020-01-24 02:41:49 +0700

    รับค่าฐานและสูงแล้วแสดงพ.ท.3เหลี่ยม

---
+++ ฝึกทำในหนังสือ/Quiz3/6.py
@@ -0,0 +1,16 @@
+
+#เขียนแบบธรรมดา
+w = float(input('Enter Your W : '))
+h = float(input('Enter Your H : '))
+HW = (w*h)/2
+print('Triangle = %.2f ' %(HW))
+
+
+#เขียนแบบ while
+hw = []
+i = 1
+while i <=2:
+     a = int(input('Enter Your Num : '))
+     hw.append(a)
+     i=i+1
+print('Trigngle = ',(hw[0]*hw[1])/2)
