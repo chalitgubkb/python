ccc9394fa4a16bfbd1eec0faf7676e4246e63b84
chalitgubkb <birdbkbbird@gmail.com>
2020-01-24 00:54:17 +0700

    รับเกรดแล้วแสดงข้อความตามเงื่อนไข

---
+++ ฝึกทำในหนังสือ/Quiz1/6.py
@@ -0,0 +1,11 @@
+c = input('Enter Your Char : ')
+if c == "A":
+    print('Excellent')
+elif c == "B":
+    print('Good')
+elif c == "C":
+    print('O.K.')
+elif c == "D":
+    print('Poor')
+elif c == 'E':
+    print('Invalid')
