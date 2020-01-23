0838e6ade383fbbedd331d221c3499258e4d0b9d
chalitgubkb <birdbkbbird@gmail.com>
2020-01-24 02:28:20 +0700

    รับค่า3ค่าแล้วหาค่าเฉลี่ย

---
+++ ฝึกทำในหนังสือ/Quiz3/5.py
@@ -0,0 +1,17 @@
+#เขียนแบบธรรมดา
+n1 = int(input('Enter Your Num1 : '))
+n2 = int(input('Enter Your Num2 : '))
+n3 = int(input('Enter Your Num3 : '))
+
+avg = (n1+n2+n3)/3
+print('AVG = ',avg)
+
+
+#เขียนแบบ while
+s = 0
+i = 1
+while i <=3:
+    n = int(input('Enter Your Num : '))
+    s = s+n
+    i = i+1
+print('AVG = ',s/3)
