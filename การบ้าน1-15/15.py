854b499501c288a0d9e9adc4a292ef938352d081
chalitgubkb <birdbkbbird@gmail.com>
2020-01-25 20:34:03 +0700

    Edit

--- การบ้าน1-15/15.py
+++ การบ้าน1-15/15.py
@@ -1,3 +1,17 @@
+#เขียนแบบธรรมดา
+n = 1
+s = 0
+while True:
+    x = int(input('Enter Your Num : '))
+    if x > s:
+        s=x
+    elif x==0:
+        break
+print('Max = ' , s)
+
+
+
+#เขียนแบบ while เก็บค่าในอาเรย์
 i = []
 while True:
     n = int(input('Enter Your Number : '))
