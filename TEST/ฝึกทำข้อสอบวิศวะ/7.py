16e933d74dc9aebb8d53184be6ce18e901f758c9
chalitgubkb <birdbkbbird@gmail.com>
2020-02-01 01:58:21 +0700

    เงื่อนไขรับสมัครเข้าทำงาน

--- TEST/ฝึกทำข้อสอบวิศวะ/7.py
+++ TEST/ฝึกทำข้อสอบวิศวะ/7.py
@@ -1,12 +1,63 @@
-text = []
-f = []
-c = input('Enter Your Char : ')
-text.append(c)
-print('Ouput = ',c)
-print(len(c))
+#ลิงค์โจทย์  https://sv1.picz.in.th/images/2020/02/01/RFjqkN.png
+name = input('Enter Your FullName: ')
+MA = float(input('Enter Score Math : '))
+TH = float(input('Enter Score Thai : '))
+EN = float(input('Enter Score English : '))
+DI = input('Enter Distance : ')
+while True:
+    SP = int(input('Enter Score Sport : '))
+    if SP > 0:
+        break
+while True:
+    MU = int(input('Enter Score Music : '))
+    if MU > 0:
+        break
+AL = MA+TH+EN
+spmu = (MU*2)+(MU*2)
+SC = []
+if AL < 100:
+    AL = 1
+    SC.append(AL)
 
-f = input('Enter Your TextFind : ')
-print(text.count(f))
+elif AL >= 100 and AL<150:
+    AL = 2
+    SC.append(AL)
 
-c2 = input('Enter Your Char2 : ')
-print(text.replace(c2))
+elif AL >= 150 and AL<200:
+    AL = 4
+    SC.append(AL)
+
+elif AL >= 200 and AL<250:
+    AL = 7
+    SC.append(AL)
+
+elif AL >= 250:
+    AL = 10
+    SC.append(AL)
+    
+
+if DI == 'A':
+        DI = 4
+        SC.append(DI)
+        DI = 'A'
+elif DI == 'B':
+        DI = 3
+        SC.append(DI)
+        DI = 'B'
+elif DI == 'C':
+        DI = 2
+        SC.append(DI)
+        DI = 'C'
+elif DI == 'D':
+        DI = 1
+        SC.append(DI)
+        DI = 'D'
+
+print('--------------------')
+print('My Name = ',name)
+print('Scouce Math+Thai+eng = ',AL)
+print('Distance = ',DI)
+print('Sport = ',SP)
+print('Music = ',MU)
+print('All Point = ',sum(SC)+spmu)
+print('--------END-------')
