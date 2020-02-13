63c26fef4d1e529007f22355826734bf2d7f99d8
chalitgubkb <birdbkbbird@gmail.com>
2020-02-14 04:19:36 +0700

    แสดงบวกลบคูฯหารโดยใช้ฟังก์ชั่น

---
+++ ฝึกทำในหนังสือ/Quiz5/1.py
@@ -0,0 +1,21 @@
+#แบบส่งค่ากลับ
+# def minus():
+#     return 20-10
+# print(minus())
+
+# #แบบไม่ส่งค่ากลับ
+# def minuss():
+#     print(20-10)
+# minuss()
+#=================================
+def minus():
+    print(20-10)
+
+def multipy():
+    print(20*10)
+
+def divide():
+    print('%.0f' %(20/10))
+minus()
+multipy()
+divide()
