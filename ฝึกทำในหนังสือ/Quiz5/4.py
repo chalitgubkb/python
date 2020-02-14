9bd1cde9c6c6b79c7519a7bca92e83b7fc6a0541
chalitgubkb <birdbkbbird@gmail.com>
2020-02-14 18:45:01 +0700

    รับค่ามาแล้วคำนวณเกรดโดยมีการส่งค่ากลับ

--- ฝึกทำในหนังสือ/Quiz5/4.py
+++ ฝึกทำในหนังสือ/Quiz5/4.py
@@ -8,12 +8,17 @@ def avg():
         b=n()
         if b >= 80 and b <= 100:
             print('A')
+            break
         elif b >= 70 and b <= 79:
             print('B')
+            break
         elif b >= 60 and b <= 69:
             print('C')
+            break
         elif b >= 50 and b <= 59:
             print('D')
+            break
         elif b >=0 and b <= 49:
             print('E')
+            break
 avg()
