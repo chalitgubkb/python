fc9d100517d5b8f211f8470bae8d26652ba5678b
chalitgubkb <birdbkbbird@gmail.com>
2020-04-02 01:41:23 +0700

    add

--- ฝึกทำในหนังสือ/Quiz8/3.py
+++ ฝึกทำในหนังสือ/Quiz8/3.py
@@ -1,6 +1,7 @@
 import os
-os.system('cls')
 os.system('color 9')
+os.system('cls')
+
 data = {32,8,9,12,14,-4,54,26,19}
 print('data = ',data)
 
@@ -14,3 +15,14 @@ print('sum = ',sum(data))
 print('union = ',data.union(data2))
 print('intersection = ',data.intersection(data2))
 print('difference = ',data.difference(data2))
+
+#ผลลัพธ์จะได้
+# data =  {32, 8, 9, 12, 14, 19, 54, 26, -4}
+# data =  {32, 80, 14, -2}
+# len =  9
+# max =  54
+# min =  -4
+# sum =  170
+# union =  {32, 8, 9, 12, 14, 80, 19, 54, 26, -4, -2}
+# intersection =  {32, 14}
+# difference =  {8, 9, 12, 19, 54, 26, -4}
